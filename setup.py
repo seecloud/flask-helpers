b645f40358e8287c6ec007594ad27642073dce72
Alexander Maretskiy <amaretskiy@mirantis.com>
2016-11-01 16:06:58 +0200

    add data_files to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ from distutils.core import setup
 setup(name="flask_helpers",
       version="0.1",
       description="Useful stuff for Flask application",
-      packages=["flask_helpers"])
+      packages=["flask_helpers"],
+      data_files=[("templates",
+                   ["flask_helpers/templates/routing_map.html"])])

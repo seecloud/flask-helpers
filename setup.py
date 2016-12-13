8248265df7dfb8a7b0ccff5467ab0dbfe1e248f6
Ilya Kharin <akscram@gmail.com>
2016-12-13 18:03:18 +0400

    Change formatting in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from distutils.core import setup
 
-setup(name="oss_lib",
-      version="0.1",
-      description="OSS Tooling Library",
-      url="https://github.com/seecloud/oss-lib",
-      author="Alexander Maretskiy",
-      author_email="amaretskiy@mirantis.com",
-      packages=["oss_lib"],
-      package_dir={"oss_lib": "oss_lib"},
-      package_data={"oss_lib": ["templates/*.html"]})
+setup(
+    name="oss_lib",
+    version="0.1.0",
+    description="OSS Tooling Library",
+    url="https://github.com/seecloud/oss-lib",
+    author="Alexander Maretskiy",
+    author_email="amaretskiy@mirantis.com",
+    packages=["oss_lib"],
+    package_dir={"oss_lib": "oss_lib"},
+    package_data={"oss_lib": ["templates/*.html"]},
+)

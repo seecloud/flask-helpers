f3c8eb34c667d8ed26e6b75198f419a8fe90f9ca
Ilya Kharin <akscram@gmail.com>
2016-12-22 17:30:28 +0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="oss_lib",
-    version="0.1.0",
+    version="0.1.1",
     description="OSS Tooling Library",
     license="Apache 2.0",
     url="https://github.com/seecloud/oss-lib",
